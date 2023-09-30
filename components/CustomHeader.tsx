7dce9d19291b20ecf66988cd70a00b6ac5bd7afc
Antonio Souza <antonios16020@gmail.com>
2023-09-29 22:58:20 -0300

    add header style

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -7,15 +7,20 @@ import Colors from '@/constants/Colors'
 const CustomHeader = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
+
       <View style={styles.container}>
         <TouchableOpacity>
           <Image style={styles.bike} source={require('@/assets/images/bike.png')} />
         </TouchableOpacity>
         <TouchableOpacity style={styles.titleContainer}>
-          <Text> Home </Text>
+          <Text style={styles.title}>Delivery · Now</Text>
+          <View style={styles.locationName}>
+            <Text style={styles.subtitle}>London</Text>
+            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+          </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.profileButton}>
-          <Ionicons name='person-outline' size={20} color={Colors.primary}/>
+          <Ionicons name="person-outline" size={20} color={Colors.primary} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -24,28 +29,70 @@ const CustomHeader = () => {
 
 const styles = StyleSheet.create({
   safeArea: {
-    flex: 1, 
-    backgroundColor: "#fff"
+    flex: 1,
+    backgroundColor: '#fff',
   },
   container: {
     height: 60,
     backgroundColor: '#fff',
     flexDirection: 'row',
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
     gap: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
   },
   bike: {
     width: 30,
-    height: 30
+    height: 30,
   },
   titleContainer: {
-
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  locationName: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   profileButton: {
-
-  }
-})
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  searchContainer: {
+    height: 60,
+    backgroundColor: '#fff',
+  },
+  searchSection: {
+    flexDirection: 'row',
+    gap: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    padding: 10,
+    color: Colors.mediumDark,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
+});
 
 export default CustomHeader
